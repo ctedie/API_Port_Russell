bf771af69a646ca524fed67e160e51fb6904e133
Cédric <cedric.tedie@gmail.com>
2026-02-18 17:37:43 +0100

    feat: protect users routes with JWT auth middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+
+const auth = require("../middleware/auth");
+router.use(auth);
+
 const usersController = require("../controllers/users.controller");
 
 // GET /users
